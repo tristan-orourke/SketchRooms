0f7aa04f03bb30033b6e9cdc1a947f12392156c1
Unknown <tristan.m.orourke@gmail.com>
2018-03-29 22:36:01 -0400

    Added generateApothem function

--- Assets/Scripts/GenerateRoomPrefab.cs
+++ Assets/Scripts/GenerateRoomPrefab.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerateRoomPrefab : MonoBehaviour
 {
 
+    public double PanelWidth;
+    public double PanelHeight;
     public GameObject PanelPrefab;
     public string RoomFolderPath = "Assets/Rooms/TestRoom";
 
@@ -12,13 +15,20 @@ public class GenerateRoomPrefab : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	    var room = GenerateRoom(RoomFolderPath, PanelPrefab, transform.position);
+	    var room = GenerateRoom(RoomFolderPath, PanelWidth, PanelHeight, PanelPrefab, transform.position);
         room.transform.SetParent(transform);
 	}
 
-    public static GameObject GenerateRoom(string roomFolderPath, GameObject panelPrefab, Vector3 origin)
+    public static GameObject GenerateRoom(string roomFolderPath, double panelWidth, double panelHeight, GameObject panelPrefab, Vector3 origin)
     {
-        var room = InstantiateCircleOfPrefabs.GenerateCircleOfPrefabs(panelPrefab, origin, 5, 20);
+        var n = 5;
+        var r = (float) CalculateApothem(n, panelWidth);
+        var room = InstantiateCircleOfPrefabs.GenerateCircleOfPrefabs(panelPrefab, origin, n, r);
         return room;
+    }
+
+    public static double CalculateApothem(int numberOfSides, double lengthOfSide)
+    {
+        return lengthOfSide / (2 * Math.Tan(Math.PI / numberOfSides));
     }
 }
